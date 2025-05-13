40cca33f550498d6906a0edd403801444a1a00d3
rishtirkulkarni <rishtirkulkarni@gmail.com>
2025-05-13 18:33:28 +0530

    Update ref.asm

--- Practice/HDL files/Project 4/ref.asm
+++ Practice/HDL files/Project 4/ref.asm
@@ -1,14 +1,6 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/4/Mult.asm
-
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-// The algorithm is based on repetitive addition.
-
-// Mult.asm
-// R2 = R0 * R1 (using repeated addition)
+// Rishti  
+// 11-05-20205
+// Multiplying numbers using repeated addition
 
 @R0
 D=M
