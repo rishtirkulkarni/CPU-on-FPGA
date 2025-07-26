616e651162b721f018e5ab6cfcad577cccf34c5b
rishtirkulkarni <rishtirkulkarni@gmail.com>
2025-07-26 23:46:22 +0530

    Update mux16.v

--- Practice/Verilog Files/Implementation Files/Project 1 - Basics/mux16.v
+++ Practice/Verilog Files/Implementation Files/Project 1 - Basics/mux16.v
@@ -1,8 +1,13 @@
 `timescale 1ns / 1ps 
 // Create Date: 03/26/2025 06:32:24 PM
-// Design Name: 16 Bit Mux (2 to1)
-// Module Name: mux16
-// Description: Created by Rishti 
+// File: mux16.v
+// Description: Behavioral description 16-bit 2-to-1 Multiplexer
+//              Selects between 2 different 16-bit inputs to route to the output line based on a 1-bit select line
+// Inputs: a, b - 16-bit inputs
+//         sel - 1-bit select line
+// Outputs: out - 16-bit output line that gets only one of the two inputs at a time
+// Author: Rishti 
+// Notes: Uses if-else statement to implement the multiplexer functionality
 
 module mux16(
     input wire [15:0] a,
