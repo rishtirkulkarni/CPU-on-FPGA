23b8c374eabde995dd79f41a85d2d6924394b51d
rishtirkulkarni <rishtirkulkarni@gmail.com>
2025-07-27 00:37:14 +0530

    Update inc16.v

--- Practice/Verilog Files/Implementation Files/Project 2 - ALU/inc16.v
+++ Practice/Verilog Files/Implementation Files/Project 2 - ALU/inc16.v
@@ -1,10 +1,13 @@
 `timescale 1ns / 1ps
  
 // Create Date: 04/02/2025 11:04:34 AM
-// Rishti 
-// Module Name: inc16 - 16 bit incrementer
-
-
+// File Name: inc16.v
+// Description: Behavioral implementation of a 16-bit incrementor
+//              Adds 1 to the 16-bit input value and produces the incremented result.
+// Inputs: in   - 16-bit input value
+// Outputs: out  - 16-bit output (input + 1)
+// Author: Rishti 
+// Notes: Simple increment operation done using '+' operator
 
 module inc16(
     input wire [15:0] in,
@@ -12,6 +15,6 @@ module inc16(
     );
     
     always @(*) begin
-        out = 16'b1 + in;
+        out = 16'b1 + in; // Increment input by 1
     end
 endmodule
