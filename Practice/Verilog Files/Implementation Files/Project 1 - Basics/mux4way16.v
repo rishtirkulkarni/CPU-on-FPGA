5413230a7a7aa6680e0308fd6589221105066d3e
rishtirkulkarni <rishtirkulkarni@gmail.com>
2025-07-26 23:55:38 +0530

    Update mux4way16.v

--- Practice/Verilog Files/Implementation Files/Project 1 - Basics/mux4way16.v
+++ Practice/Verilog Files/Implementation Files/Project 1 - Basics/mux4way16.v
@@ -1,10 +1,15 @@
 `timescale 1ns / 1ps
 
 // Create Date: 04/26/2025 
-// Rishti 
-// Module Name: mux4way16
-
-
+// File Name: mux4way16
+// Description: Behavioral implementation of 16-bit 4-to-1 Multiplexer 
+//              Selects between 4 different 16-bit inputs to route to the output line based on a 2-bit select line
+// Inputs: a, b, c, d - 16-bit inputs
+//         sel - 2-bit select line
+// Outputs: out - 16-bit output that gets only one of the 4 inputs at a time
+// Author: Rishti 
+// Notes: Case statement has been used to select different inputs to route to the output line
+//        based on the 2-bit select line
 
 module mux4way16(
     input [15:0] a, b, c, d,
