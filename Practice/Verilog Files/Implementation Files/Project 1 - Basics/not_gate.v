3472fa307fba878cb93ef8c695e3ca8b9f78a8cf
rishtirkulkarni <rishtirkulkarni@gmail.com>
2025-07-26 23:16:42 +0530

    Update not_gate.v

--- Practice/Verilog Files/Implementation Files/Project 1 - Basics/not_gate.v
+++ Practice/Verilog Files/Implementation Files/Project 1 - Basics/not_gate.v
@@ -1,9 +1,13 @@
 `timescale 1ns / 1ps
 
 // Create Date: 03/24/2025 04:41:56 PM
-// Design Name: NOT Gate
-// Module Name: not_gate 
-// Description: Created by Rishti
+// File: not_gate.v
+// Description: Behavioral implementation of a 1-bit NOT gate.
+// Inputs:
+//    a   - Single-bit input
+// Outputs:
+//    y   - Output (logical inversion of input)
+// Created by Rishti
  
 
 module not_gate(
