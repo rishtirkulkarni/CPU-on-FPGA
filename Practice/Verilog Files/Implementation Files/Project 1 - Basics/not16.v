d231b91ad289a1e437b0f8bac37950c3f321bf21
rishtirkulkarni <rishtirkulkarni@gmail.com>
2025-07-27 00:06:29 +0530

    Update not16.v

--- Practice/Verilog Files/Implementation Files/Project 1 - Basics/not16.v
+++ Practice/Verilog Files/Implementation Files/Project 1 - Basics/not16.v
@@ -1,9 +1,12 @@
 `timescale 1ns / 1ps 
 // Create Date: 03/26/2025 09:09:15 AM
-// Design Name: 16 bit NOT gate
-// Module Name: not16 
-// Description: Created by Rishti
-
+// File: not16.v
+// Description: Behavioral description of a 16-bit not gate
+//              Performs bitise inversion on each bit of a 16-bit input
+// Input: in - 16-bit input
+// Output: out - 16-bit output (bitwise complement of input a)
+// Author: Rishti
+// Notes: Uses a for loop or bitwise NOT operator for inversion.
 
 module not16(
     input [15:0] in,
@@ -11,9 +14,10 @@ module not16(
     
     );
     
-    integer i;
+    integer i; // Loop variable for iterating through bits
     
     always @(*) begin
+        // Iterate through each bit and perform NOT operation
         for (i=0; i<16; i=i+1)begin
             if (in[i] == 1'b1)
                 out[i] = 1'b0;
