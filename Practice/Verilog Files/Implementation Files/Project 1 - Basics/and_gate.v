c52817468db7a61b20d47498b156eafc462ff2b8
rishtirkulkarni <rishtirkulkarni@gmail.com>
2025-07-26 23:14:33 +0530

    Update and_gate.v

--- Practice/Verilog Files/Implementation Files/Project 1 - Basics/and_gate.v
+++ Practice/Verilog Files/Implementation Files/Project 1 - Basics/and_gate.v
@@ -1,8 +1,12 @@
 `timescale 1ns / 1ps
 // Create Date: 03/24/2025 05:15:48 PM
-// Design Name: AND gate
-// Module Name: and_gate
-// Description: Created by Rishti
+// File: and_gate.v
+// Description: Behavioral implementation of a 1-bit AND gate.
+// Inputs:
+//    a, b   - Single-bit inputs
+// Outputs:
+//    y      - Output (1 if both inputs are high)
+// Created by Rishti
 
 
 
@@ -13,7 +17,7 @@ module and_gate(
     );
     
     always @(*) begin
-        if (a == 1'b1 && b == 1'b1)
+        if (a == 1'b1 && b == 1'b1) //Conditional statement can also be replaced by '&' operator.
             y = 1'b1;
         else 
             y = 1'b0;
