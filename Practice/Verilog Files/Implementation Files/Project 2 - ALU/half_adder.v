fdef9b90b796d3f9db69ac6792fd1a514e225d27
rishtirkulkarni <rishtirkulkarni@gmail.com>
2025-07-27 00:19:21 +0530

    Update half_adder.v

--- Practice/Verilog Files/Implementation Files/Project 2 - ALU/half_adder.v
+++ Practice/Verilog Files/Implementation Files/Project 2 - ALU/half_adder.v
@@ -1,10 +1,14 @@
 `timescale 1ns / 1ps
  
 // Create Date: 03/31/2025 05:05:19 PM
-// Rishti
-// Module Name: half_adder
-
-
+// Module Name: half_adder.v
+// Description: Behavioral description of a 1-bit half adder
+//              Adds 2 inputs, produces a sum and carry outpts
+// Inputs: a, b - 1-bit inputs
+// Outputs: sum - 1-bit sum output
+//          c_out - 1-bit carry output
+// Author: Rishti
+// Notes: Logical operators and procedural assignments have been used to implement the half adder functionaity
 
 module half_adder(
     input wire a,
@@ -14,7 +18,7 @@ module half_adder(
     );
     
     always @(*)begin
-        sum = a ^ b;
-        c_out = a & b;
+        sum = a ^ b;     // Performs EXOR of inputs to produce sum bit
+        c_out = a & b;   // Performs AND of inputs to produce carry output bit
     end
 endmodule
