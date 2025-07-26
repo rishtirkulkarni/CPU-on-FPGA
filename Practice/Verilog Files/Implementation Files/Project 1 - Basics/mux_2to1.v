4375a3d0030c4cb1ad9038a734e98f743c11d4f7
rishtirkulkarni <rishtirkulkarni@gmail.com>
2025-07-27 00:00:28 +0530

    Update mux_2to1.v

--- Practice/Verilog Files/Implementation Files/Project 1 - Basics/mux_2to1.v
+++ Practice/Verilog Files/Implementation Files/Project 1 - Basics/mux_2to1.v
@@ -1,10 +1,13 @@
 `timescale 1ns / 1ps
 
 // Create Date: 03/25/2025 08:18:01 PM
-// Design Name: 2 to 1 Mux
-// Module Name: mux_2to1 
-// Description: Created by Rishti
-
+// File: mux_2to1.v
+// Description: Behavioral description of a 2-to-1 Multiplexer
+//              Selects between 2 1-bit inputs to route to the output line based on a 1-bit select line
+// Inputs: a, b - 1-bit inputs
+//         sel - 1-bit select line
+// Outputs: y - 1-bit output line that gets one of the inputs at a time
+// Author: Rishti
 
 module mux_2to1(
     input wire a,
@@ -14,8 +17,8 @@ module mux_2to1(
     );
     
     always @(*) begin
-        if (sel == 1'b0)
-            y = a;
+        if (sel == 1'b0) // If-else statements used to select different inputs
+            y = a;       // based on different values of the select line
         else 
             y = b;
     end
