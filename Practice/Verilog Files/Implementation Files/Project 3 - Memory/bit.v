2b386d3b687d9e9d7b0be2fbc3864e4607cd814d
rishtirkulkarni <rishtirkulkarni@gmail.com>
2025-07-28 12:00:12 +0530

    Update bit.v

--- Practice/Verilog Files/Implementation Files/Project 3 - Memory/bit.v
+++ Practice/Verilog Files/Implementation Files/Project 3 - Memory/bit.v
@@ -1,8 +1,16 @@
 `timescale 1ns / 1ps
  
 // Create Date: 04/24/2025 07:23:30 PM
-// Rishti 
-// 1 Bit register
+// File: bit.v
+// Description: 1-bit register with load control.
+// Stores a single bit on the rising edge of the clock when load is high.
+// Inputs:
+//    in    - Data input
+//    load  - Load control (when high, captures 'in' on clock edge)
+//    clk   - Clock input
+// Outputs:
+//    out   - Stored bit value
+// Author: Rishti
 
 module bit(
     input wire in,
@@ -13,7 +21,8 @@ module bit(
     
     always@(posedge clk)begin
         if(load)
-            out <= in;
+            out <= in; // Capture input when load is high
+            // Otherwise, retain the previous value of out
     end
     
 endmodule
